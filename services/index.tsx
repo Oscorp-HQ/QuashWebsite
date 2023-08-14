c3cbcade79724d117e577da4df8ce54cf378ca25
droiddumbledore <136621354+droiddumbledore@users.noreply.github.com>
2023-08-15 00:36:11 +0530

    Update index.tsx

--- services/index.tsx
+++ services/index.tsx
@@ -4,7 +4,7 @@ const BASE_URL = "https://quash-v1.el.r.appspot.com";
 
 export const PostContact = (data: any) => {
   return Api()
-    .post(`${BASE_URL}/api/contacts`, data)
+    .post(`${BASE_URL}/api/contacts/`, data)
     .then((result: any) => {
       return result.data;
     });
