7061299b046586a204cc770342139f48225f4111
Hemanthr1 <hemanth@quashbugs.com>
2023-09-26 19:09:00 +0530

    removed unwanted components and changes mob images

--- components/resolution/index.tsx
+++ components/resolution/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const resoltionMedia = ["/context.svg", "/crash.svg", "/ai.svg"];
+const resoltionMedia = ["context-mob.svg", "crash-mob.svg", "ai-mob.svg"];
 
 const Resolution = () => {
   return (
