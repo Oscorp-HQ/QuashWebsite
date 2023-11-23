c1429444bd4cee9c83b575fd98ed00563fd785b3
ShivamQuashbugs <shivam@quashbugs.com>
2023-11-23 15:11:41 +0530

    added h2 tags in homepage

--- components/comparision/index.tsx
+++ components/comparision/index.tsx
@@ -5,9 +5,9 @@ const Comparision = () => {
   return (
     <div className="w-full px-[121px] md:flex hidden pt-[248px] pb-[48px]">
       <div className="flex flex-col w-full gap-12 text-center justify-center items-center">
-        <span className="text-[42px] text-[#FFFFFF] font-[500] leading-normal">
+        <h2 className="text-[42px] text-[#FFFFFF] font-[500] leading-normal">
           Enable your team to ship bug-free releases faster
-        </span>
+        </h2>
         <Image
           src="/comparision.svg"
           alt="comparision"
