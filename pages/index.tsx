cfea7dd3f479d4c39945f7bd05e341c045e944e2
Hemanthr1 <hemanth@quashbugs.com>
2023-12-22 08:48:45 +0530

    OG meta tags added on home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,32 @@ export default function Home() {
           content="Bug & Crash Reporting for Mobile Developers"
           key="desc"
         />
+        <meta property="image" content="/open-graph.png" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://quashbugs.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="🐞 Quash - Streamlining QA Processes 🚀"
+        />
+        <meta
+          property="og:description"
+          content="Bug & Crash Reporting for Mobile Developers"
+        />
+        <meta property="og:image" content="/open-graph.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="🐞 Quash - Streamlining QA Processes 🚀"
+        />
+        <meta
+          name="twitter:description"
+          content="Bug & Crash Reporting for Mobile Developers"
+        />
+        <meta name="twitter:image" content="/open-graph.png" />
       </Head>
       <Hero />
       <Comparision />
