47d8c6e325652ba12f60da4850e2d18dafcef429
Shivam <shivam@quashbugs.com>
2024-01-30 21:38:47 +0530

    images in resolution component replaced for mobile screen

--- components/resolution/index.tsx
+++ components/resolution/index.tsx
@@ -3,21 +3,19 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-
 const resoltionMedia = [
-  {src:"context-mob.svg",
-   label:"context-mob" }, {src:"crash-mob.svg",
-  label:"crash-mob"},
-  {src:"ai-new-mob.svg",
-  label:"ai-mob"}];
+  { src: "context-resolution-mob.svg", label: "context-mob" },
+  { src: "crash-resolution-mob.svg", label: "crash-mob" },
+  { src: "ai-resolution-mob.svg", label: "ai-mob" },
+];
 
 const Resolution = () => {
   return (
     <div className="flex px-4 pb-16 md:pb-12  flex-col justify-center items-center">
-    <div className="relative w-full flex justify-center items-center flex-col overflow-hidden">
-      <div className="grad-resolution-main h-[150px] w-[192px] md:h-[400px] md:w-[400px] opacity-[34%] md:opacity-[20%]"></div>
-      <div className="w-full h-[48px] md:h-[200px] absolute top-0 bg-black"></div>
-      <div className="absolute top-[48px] md:top-[200px] flex flex-col justify-center items-center gap-12 md:gap-[116px]">
+      <div className="relative w-full flex justify-center items-center flex-col overflow-hidden">
+        <div className="grad-resolution-main h-[150px] w-[192px] md:h-[400px] md:w-[400px] opacity-[34%] md:opacity-[20%]"></div>
+        <div className="w-full h-[48px] md:h-[200px] absolute top-0 bg-black"></div>
+        <div className="absolute top-[48px] md:top-[200px] flex flex-col justify-center items-center gap-12 md:gap-[116px]">
           <div className="resolution-gradient-line h-[2px] w-[208px] md:h-[4px] md:w-[400px]" />
           <span className="resolution-gradient-text text-[32px] md:text-[60px] md:font-[600] leadind-normal">
             Resolution
@@ -43,7 +41,13 @@ const Resolution = () => {
           height={598}
           className=""
         />
-        <Image src="/ai-new.svg" alt="ai" width={1270} height={326} className="" />
+        <Image
+          src="/ai-new.svg"
+          alt="ai"
+          width={1270}
+          height={326}
+          className=""
+        />
       </div>
       <Carousel
         // autoPlay
@@ -58,15 +62,15 @@ const Resolution = () => {
         {resoltionMedia.map((media, index) => (
           <>
             <Image
-            key={index}
-            src={media.src}
-            alt={media.label}
-            width={272}
-            height={374}
-            className=""
-          />
-          <br />
-          <br />
+              key={index}
+              src={media.src}
+              alt={media.label}
+              width={272}
+              height={374}
+              className=""
+            />
+            <br />
+            <br />
           </>
         ))}
       </Carousel>
